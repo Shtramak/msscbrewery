23a06ceed91a6069e0881fbce34a9fba20019819
shtramak <shtramak@gmail.com>
2019-09-21 21:40:16 +0300

    add POST/PUT/DELETE mappings to BeerController

--- src/main/java/com/shtramak/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/shtramak/msscbrewery/web/controller/BeerController.java
@@ -2,11 +2,17 @@ package com.shtramak.msscbrewery.web.controller;
 
 import com.shtramak.msscbrewery.web.model.BeerDto;
 import com.shtramak.msscbrewery.web.service.BeerService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -21,7 +27,28 @@ public class BeerController {
     }
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
+    public ResponseEntity<BeerDto> getById(@PathVariable UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody BeerDto beerDto) {
+        BeerDto savedBeer = beerService.save(beerDto);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location", "/api/v1/beers/" + savedBeer.getId());
+        return new ResponseEntity<>(savedBeer, httpHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void update(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+        beerService.update(beerDto);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable UUID beerId) {
+        beerService.deleteById(beerId);
+    }
+
 }
--- src/main/java/com/shtramak/msscbrewery/web/service/BeerServiceImpl.java
+++ src/main/java/com/shtramak/msscbrewery/web/service/BeerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.shtramak.msscbrewery.web.service;
 
 import com.shtramak.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
@@ -14,5 +16,22 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDto save(BeerDto beerDto) {
+        UUID generatedId = UUID.randomUUID();
+        beerDto.setId(generatedId);
+        return beerDto;
+    }
+
+    @Override
+    public void update(BeerDto beerDto) {
+        log.info("update method running...");
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.info("delete method running...");
     }
 }
