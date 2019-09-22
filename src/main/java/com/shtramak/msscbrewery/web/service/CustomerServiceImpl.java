87b42870ac8d8fd39e51ab5072df7a1686b4f6d3
shtramak <shtramak@gmail.com>
2019-09-22 12:47:13 +0300

    add POST/PUT/DELETE mappings to CustomerController

--- src/main/java/com/shtramak/msscbrewery/web/service/CustomerServiceImpl.java
+++ src/main/java/com/shtramak/msscbrewery/web/service/CustomerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.shtramak.msscbrewery.web.service;
 
 import com.shtramak.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
@@ -13,5 +15,23 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(customerId)
                 .name("John")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl
+        log.debug("Updating....");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting.... ");
     }
 }
